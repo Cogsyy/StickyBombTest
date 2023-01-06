14c608cafacb4921652498f47bdd97e737cdd482
Jordan <jordancog94@gmail.com>
2023-01-06 13:55:44 -0500

    added ammo counter to the hud

---
+++ Source/StickyBombTest/StickyBombPlayerState.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StickyBombPlayerState.h"
+
+void AStickyBombPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeAmmo(AmmoStarting);
+}
+
+void AStickyBombPlayerState::ChangeAmmo(int DeltaChange)
+{
+	const int MaxAmmo = 9999;
+	AmmoCurrent = FMath::Clamp(AmmoCurrent + DeltaChange, 0, MaxAmmo);
+
+	OnAmmoCountChanged.Broadcast(AmmoCurrent);
+	UE_LOG(LogTemp, Log, TEXT("WeaponComp: Ammo is now %i"), AmmoCurrent);
+}
---
+++ Source/StickyBombTest/StickyBombPlayerState.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "StickyBombPlayerState.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAmmoCountChanged, float, NewAmmo);
+
+/**
+ * 
+ */
+UCLASS()
+class STICKYBOMBTEST_API AStickyBombPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+	void ChangeAmmo(int DeltaChange);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnAmmoCountChanged OnAmmoCountChanged;
+
+private:
+	UPROPERTY(EditAnywhere)
+	int AmmoStarting = 3;
+	
+	int AmmoCurrent;
+};
